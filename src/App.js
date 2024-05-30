4c6251bd9919fae5031bc244317c86c2e7e91b2e
王哲玮 <m13636472690@163.com>
2024-05-31 01:34:38 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LandingPage from "./ui/landing-page/LandingPage";
+import MainPage from "./ui/landing-page/MainPage";
+
+function App() {
+  return <MainPage />;
+}
+
+export default App;
---
+++ src/ui/landing-page/database.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import MyImageList from "./components/MyImageList";
+import SearchBar from "./components/SearchBar";
+import { Container } from "@mui/material";
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Database() {
+  return (
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          mx: 32,
+          mt: 4,
+        }}
+      >
+        <h1>三星堆数据库</h1>
+        <SearchBar />
+      </Box>
+      <MyImageList />
+    </Container>
+  );
+}
