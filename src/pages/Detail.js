fc81cca801d707fb7ef61174f1d4f628c6323cb5
王哲玮 <m13636472690@163.com>
2024-06-01 17:06:11 +0800

    update detail page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -6,7 +6,6 @@ import {
   Container,
   Box,
   Typography,
-  Grid,
   Card,
   CardContent,
   CardMedia,
@@ -20,29 +19,45 @@ const Detail = () => {
     <Container>
       <Box sx={{ my: 4 }}>
         {item ? (
-          <Card sx={{ display: "flex" }}>
-            <CardMedia
-              component="img"
-              sx={{ width: 300 }}
-              image={`${item.img}?w=248&fit=crop&auto=format`}
-              alt={item.name}
-            />
-            <Box sx={{ display: "flex", flexDirection: "column" }}>
-              <CardContent sx={{ flex: "1 0 auto" }}>
-                <Typography component="div" variant="h5">
-                  {item.name}
-                </Typography>
-                <Typography
-                  variant="subtitle1"
-                  color="text.secondary"
-                  component="div"
-                >
-                  Year: {item.year}
-                </Typography>
-                <Typography variant="body1">{item.description}</Typography>
-              </CardContent>
+          <Box sx={{ display: "flex" }}>
+            <Box>
+              <Card sx={{ display: "flex", mb: 2 }}>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 300 }}
+                  image={`${item.img}?w=248&fit=crop&auto=format`}
+                  alt={item.name}
+                />
+              </Card>
             </Box>
-          </Card>
+            <Box sx={{ spacing: 2, mx: 2 }}>
+              <Card sx={{ mb: 2 }}>
+                <CardContent>
+                  <Typography component="div" variant="h5">
+                    {item.name}
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card sx={{ mb: 2 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    年代
+                  </Typography>
+                  <Typography variant="subtitle1" component="div">
+                    {item.year}
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card sx={{ mb: 2 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    描述
+                  </Typography>
+                  <Typography variant="body1">{item.description}</Typography>
+                </CardContent>
+              </Card>
+            </Box>
+          </Box>
         ) : (
           <Typography variant="h6" align="center">
             Item not found
