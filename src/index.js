6c926244340ea2fdaa0039eef91e0b8ff342cdd3
王哲玮 <m13636472690@163.com>
2024-06-02 15:59:01 +0800

    update createBrowserRouter

--- src/index.js
+++ src/index.js
@@ -7,30 +7,57 @@ import Explore from "./pages/Explore";
 import Database from "./pages/Database";
 import NoPage from "./pages/NoPage";
 import Detail from "./pages/Detail";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/Sanxingdui-DigitalMuseum",
+    element: <Layout />,
+    children: [
+      {
+        path: "/Sanxingdui-DigitalMuseum",
+        element: <Home />,
+      },
+      {
+        path: "/Sanxingdui-DigitalMuseum/home",
+        element: <Home />,
+      },
+      {
+        path: "/Sanxingdui-DigitalMuseum/explore",
+        element: <Explore />,
+      },
+      {
+        path: "/Sanxingdui-DigitalMuseum/database",
+        element: <Database />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/Sanxingdui-DigitalMuseum" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/Sanxingdui-DigitalMuseum/home" element={<Home />} />
-          <Route
-            path="/Sanxingdui-DigitalMuseum/explore"
-            element={<Explore />}
-          />
-          <Route
-            path="/Sanxingdui-DigitalMuseum/database"
-            element={<Database />}
-          />
-          <Route
-            path="/Sanxingdui-DigitalMuseum/detail/:id"
-            element={<Detail />}
-          />
-          <Route path="/Sanxingdui-DigitalMuseum/*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/Sanxingdui-DigitalMuseum" element={<Layout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="/Sanxingdui-DigitalMuseum/home" element={<Home />} />
+    //       <Route
+    //         path="/Sanxingdui-DigitalMuseum/explore"
+    //         element={<Explore />}
+    //       />
+    //       <Route
+    //         path="/Sanxingdui-DigitalMuseum/database"
+    //         element={<Database />}
+    //       />
+    //       <Route
+    //         path="/Sanxingdui-DigitalMuseum/detail/:id"
+    //         element={<Detail />}
+    //       />
+    //       <Route path="/Sanxingdui-DigitalMuseum/*" element={<NoPage />} />
+    //     </Route>
+    //   </Routes>
+    // </Router>
+    <RouterProvider router={router} />
   );
 }
 
