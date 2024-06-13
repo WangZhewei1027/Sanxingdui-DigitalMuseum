1659178856b777b3b881d9fae45d015ffb5423e4
王哲玮 <m13636472690@163.com>
2024-06-13 23:33:19 +0800

    Update Detail.js

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -12,6 +12,38 @@ import {
 } from "@mui/material";
 import { spacing } from "@mui/system";
 
+function MyCard({ text, caption, fontSize = "h3" }) {
+  return (
+    <React.Fragment>
+      <Card
+        sx={{
+          display: "flex",
+          flexGrow: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+        }}
+      >
+        <CardContent>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant={fontSize}>{text}</Typography>
+            <Typography variant="caption" sx={{ mt: 2 }}>
+              {caption}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    </React.Fragment>
+  );
+}
+
 const Detail = () => {
   const { id } = useParams();
   const item = data.find((item) => item.id === id);
@@ -103,7 +135,7 @@ const Detail = () => {
                       }}
                     >
                       <Typography
-                        variant="h1"
+                        variant="h2"
                         align="center"
                         sx={{ fontWeight: "bold" }}
                       >
@@ -152,180 +184,138 @@ const Detail = () => {
             </Grid>
 
             {/* Right */}
-            <Grid container item xs={12} md={4} spacing={2}>
-              {/* Right Middle */}
-              <Grid container item xs={12} md={12} spacing={2}>
-                <Grid item xs={6} md={6} spacing={2}>
-                  <Box
-                    display={"flex"}
-                    flexDirection={"Column"}
-                    gap={2}
-                    height={"100%"}
-                  >
-                    <Card
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        flexGrow: 1,
-                      }}
-                    >
-                      <CardContent>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "center",
-                          }}
-                        >
-                          <Typography variant="h3">商</Typography>
-                          <Typography variant="caption" sx={{ mt: 2 }}>
-                            年代
-                          </Typography>
-                        </Box>
-                      </CardContent>
-                    </Card>
-
-                    <Card
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        flexGrow: 1,
-                      }}
-                    >
-                      <CardContent>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "center",
-                          }}
-                        >
-                          <Typography variant="h3">{item.hierarchy}</Typography>
-                          <Typography variant="caption" sx={{ mt: 2 }}>
-                            文物级别
-                          </Typography>
-                        </Box>
-                      </CardContent>
-                    </Card>
-
-                    <Card
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        flexGrow: 1,
-                      }}
-                    >
-                      <CardContent>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "center",
-                          }}
-                        >
-                          <Typography variant="h3">{item.material}</Typography>
-                          <Typography variant="caption" sx={{ mt: 2 }}>
-                            材料
-                          </Typography>
-                        </Box>
-                      </CardContent>
-                    </Card>
-                  </Box>
-                </Grid>
-
-                <Grid item xs={6} md={6}>
-                  <Card sx={{ height: "100%" }}>
-                    <CardContent>
-                      <Typography variant="h5">
-                        细节相册细节相册细节相册细节相册细节相册细节相册细节相册细节相册细节相册细节相册细节相册细节相册细节相册细节细节相册细节相册细节相册细节相册细节相册细节相册细节相册相册细节相册细节相册细节相册细节相册
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-
-              {/* Right Bottom */}
-              <Grid
-                container
-                item
-                xs={12}
-                md={12}
-                spacing={2}
-                sx={{ background: "rgba(255,255,255,0.2)" }}
+            <Grid item xs={12} md={4} flexDirection={"column"}>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                sx={{ height: "100%" }}
+                gap={2}
               >
-                <Grid item xs={7} md={7}>
-                  <Card>
-                    <CardContent>
+                {/* Right Middle */}
+                <Box display={"flex"}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6} md={6}>
                       <Box
-                        sx={{
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
+                        display={"flex"}
+                        flexDirection={"Column"}
+                        gap={2}
+                        height={"100%"}
                       >
-                        <Typography variant="h6">{item.category}</Typography>
-                        <Typography variant="caption" sx={{ marginTop: 1 }}>
-                          类别
-                        </Typography>
+                        <Card
+                          sx={{
+                            display: "flex",
+                            flexGrow: 1,
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <CardContent>
+                            <Box
+                              sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                              }}
+                            >
+                              <Typography variant="h3">商</Typography>
+                              <Typography variant="caption" sx={{ mt: 2 }}>
+                                年代
+                              </Typography>
+                            </Box>
+                          </CardContent>
+                        </Card>
+
+                        <Card
+                          sx={{
+                            display: "flex",
+                            flexGrow: 1,
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <CardContent>
+                            <Box
+                              sx={{
+                                display: "flex",
+                                flexGrow: 1,
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                              }}
+                            >
+                              <Typography variant="h3">
+                                {item.hierarchy}
+                              </Typography>
+                              <Typography variant="caption" sx={{ mt: 2 }}>
+                                文物级别
+                              </Typography>
+                            </Box>
+                          </CardContent>
+                        </Card>
+
+                        <Card
+                          sx={{
+                            display: "flex",
+                            flexGrow: 1,
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <CardContent>
+                            <Box
+                              sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                              }}
+                            >
+                              <Typography variant="h3">
+                                {item.material}
+                              </Typography>
+                              <Typography variant="caption" sx={{ mt: 2 }}>
+                                材料
+                              </Typography>
+                            </Box>
+                          </CardContent>
+                        </Card>
                       </Box>
-                    </CardContent>
-                  </Card>
-                </Grid>
-                <Grid item xs={5} md={5}>
-                  <Card>
-                    <CardContent>
-                      <Box
-                        sx={{
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
-                      >
-                        <Typography variant="h6">XX制造方法</Typography>
-                        <Typography variant="caption" sx={{ marginTop: 1 }}>
-                          铸造方法
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </Grid>
-                <Grid item xs={5}>
-                  <Card>
-                    <CardContent>
-                      <Box
-                        sx={{
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
-                      >
-                        <Typography variant="h4">n号坑</Typography>
-                        <Typography variant="caption" sx={{ marginTop: 1 }}>
-                          出土坑
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </Grid>
-                <Grid item xs={7} height={"100%"}>
-                  <Card height={"100%"}>
-                    <CardContent>
-                      <Typography variant="h6">
-                        一些文字一些文字一些文字
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
+                    </Grid>
+
+                    <Grid item xs={6} md={6}>
+                      <Card sx={{ height: "100%" }}>
+                        <CardContent>
+                          <Typography variant="h5">
+                            相册细节相册细节相册细节相册细节细节相册细节相册细节相册细节相册细节相册细节相册细节相册相册细节相册细节相册细节相册细节相册
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  </Grid>
+                </Box>
+                {/* Right Bottom */}
+                <Box display={"flex"} flexGrow={1}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={7} md={7}>
+                      <MyCard
+                        text="XX制造方法"
+                        caption="制造方法"
+                        fontSize="h4"
+                      />
+                    </Grid>
+                    <Grid item xs={5} md={5}>
+                      <MyCard text={item.category} caption="类别" />
+                    </Grid>
+                    <Grid item xs={5}>
+                      <MyCard text="n号坑" caption="出土坑位" />
+                    </Grid>
+                    <Grid item xs={7}>
+                      <MyCard text="一些文字" />
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
             </Grid>
           </>
         ) : (
